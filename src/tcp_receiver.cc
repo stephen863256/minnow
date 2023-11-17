63f5c17de790a4759334fe49c8efe16213898c72
PB21111726 <1324366487@qq.com>
2023-11-17 15:24:05 +0800

    finish

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,4 +1,5 @@
 #include "tcp_receiver.hh"
+#include "wrapping_integers.hh"
 
 using namespace std;
 
@@ -8,11 +9,33 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
   (void)message;
   (void)reassembler;
   (void)inbound_stream;
+  if(!isn_.has_value())
+  {
+    if(!message.SYN)
+    {
+      return;
+    }
+    isn_ = message.seqno;
+  }
+  auto checkpoint = inbound_stream.bytes_pushed() + 1;
+  auto abs_seqno = message.seqno.unwrap(isn_.value(),checkpoint);
+
+  auto first_index = message.SYN ? 0 : abs_seqno - 1;
+  reassembler.insert(first_index, message.payload, message.FIN, inbound_stream);
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   // Your code here.
   (void)inbound_stream;
-  return {};
+  TCPReceiverMessage msg;
+  auto win_s = inbound_stream.available_capacity();
+  msg.window_size = win_s > UINT16_MAX ? UINT16_MAX : win_s;
+  if(isn_.has_value())
+  {
+    auto abs_seqno = inbound_stream.bytes_pushed() + 1 + inbound_stream.is_closed();
+    msg.ackno = Wrap32::wrap(abs_seqno,isn_.value());
+  }
+  
+  return msg;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,25 @@
 #include "wrapping_integers.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  static constexpr uint64_t TWO31 = 1UL << 31;
+  static constexpr uint64_t TWO32 = 1UL << 32;
+
+  auto const ckpt = Wrap32::wrap(checkpoint, zero_point);
+  uint64_t bias = raw_value_ - ckpt.raw_value_;
+  if(bias <= TWO31 || checkpoint + bias < TWO32)
+  {
+    return checkpoint + bias; 
+  }
+  return checkpoint + bias - TWO32;
 }
